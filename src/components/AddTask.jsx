f5cc2782ab0fcfe23695e78f50ec65ff37851228
GoranKim0518 <sungmin051830@gmail.com>
2025-05-06 02:03:56 +0900

    fix(icon): import FontAwesome icon

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,3 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 export default function AddTask() {
-    return('');
+    function handleClick(event) {
+        console.log(event);
+    }
+    return(
+        <button onClick={(event) => handleClick(event)}>
+            <FontAwesomeIcon icon={faPlus} />
+            What's your focus for today?
+        </button>
+    );
 }
