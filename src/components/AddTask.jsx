ee5d5c8d7dd8b42c47ea6b1c8953a214a43087dc
GoranKim0518 <sungmin051830@gmail.com>
2025-05-06 10:32:41 +0900

    feat(AddTask): add list with its dummy data

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,14 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
+//dummy tasks; It will be replaced with backend API.
+const dumyTasks = [
+    {id: 1, task: "Learn React"},
+    {id: 2, task: "Make To-Do app"},
+    {id: 3, task: "Test the application"},
+]
+
 export default function AddTask() {
     function handleClick(event) {
         console.log(event);
     }
     return(
-        <button onClick={(event) => handleClick(event)}>
-            <FontAwesomeIcon icon={faPlus} />
-            What's your focus for today?
-        </button>
+        <div>
+            <button onClick={(event) => handleClick(event)}>
+                <FontAwesomeIcon icon={faPlus} />
+                <p>What's your focus for today?</p>
+            </button>
+            {dumyTasks.map((item) => <li key={item.id}>{item.task}</li>)}
+        </div>
     );
 }
