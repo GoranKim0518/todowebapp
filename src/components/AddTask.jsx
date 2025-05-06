e8190befe25a3a159d4dde431d6bbe1155775eeb
GoranKim0518 <sungmin051830@gmail.com>
2025-05-06 17:18:04 +0900

    feat(new task): add functions that  insert new tasks and display them

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,24 +1,36 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-//dummy tasks; It will be replaced with backend API.
-const dumyTasks = [
-    {id: 1, task: "Learn React"},
-    {id: 2, task: "Make To-Do app"},
-    {id: 3, task: "Test the application"},
-]
+import InsertNewTask from "./InsertNewTask";
 
 export default function AddTask() {
+    const [tasks, setTasks] = useState([
+        { id: 1, task: "Learn React" },
+        { id: 2, task: "Make To-Do app" },
+        { id: 3, task: "Test the application" },
+    ]);
+
     function handleClick(event) {
         console.log(event);
     }
+
+    //remain previous tasks while adding new task in array
+    function handleAddTask(newTask) {
+        const insertedTask = {id: tasks.length + 1, task: newTask};
+        setTasks([...tasks, insertedTask]);
+    }
+
+    console.log(tasks);
+
     return(
         <div>
             <button onClick={(event) => handleClick(event)}>
                 <FontAwesomeIcon icon={faPlus} />
                 <p>What's your focus for today?</p>
             </button>
-            {dumyTasks.map((item) => <li key={item.id}>{item.task}</li>)}
+            {tasks.map((item) => <ul><li key={item.id}>{item.task}</li></ul>)}
+            <InsertNewTask onAddTask={handleAddTask}/>
         </div>
     );
 }
--- src/components/InsertNewTask.jsx
+++ src/components/InsertNewTask.jsx
@@ -1,4 +1,23 @@
-export default function InsertNewTask() {
-    <div></div>
-    return('');
+import { useState } from "react";
+
+export default function InsertNewTask({ onAddTask }) {
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(taskValue) {
+        setNewTask(taskValue);
+    }
+
+    function handleNewTask() {
+        if(newTask.trim().length === 0) return;
+        onAddTask(newTask);     //transport newTask to upper level; AddTask.jsx
+        setNewTask("");     //init newTask as it only functions transporting 'newly inserted' task
+        console.log(newTask);
+    }
+
+    return(
+        <div>
+            <input type="text" placeholder="Enter a new Task" value={newTask} onChange={(event) => handleInputChange(event.target.value)}></input>
+            <button onClick={handleNewTask}>submit</button>
+        </div>
+    );
 }
