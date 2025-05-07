2faa79fdb2da39ed0f2fa152988d6ff480be3b96
GoranKim0518 <sungmin051830@gmail.com>
2025-05-07 19:03:51 +0900

    feat(dummy bar): add dummy completedTask bar

--- src/components/CompletedTask.jsx
+++ src/components/CompletedTask.jsx
@@ -1,3 +1,13 @@
 export default function CompletedTask() {
-    return('');
+    return(
+        <div>
+            <div>
+                <div>2</div>
+                <div>completed Task</div>
+            </div>
+            <div>
+                <i class="fa-solid fa-trash-can"></i>
+            </div>
+        </div>
+    );
 }
